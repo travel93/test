b6f768a47dbb74e0085c42bba620701fbb9113e0
ClownL12 <928287517@qq.com>
2021-05-30 19:43:10 +0800

    注册界面材料提交

---
+++ src/servlet/LoginServlet.java
@@ -0,0 +1,31 @@
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @Description:
+ * @author: ClownL12
+ * @create: 2021-05-29 14:57
+ */
+@WebServlet(name = "LoginServlet",urlPatterns = "/login")
+public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("utf-8");
+        response.setContentType("text/html;charset=utf-8");
+
+        String name = request.getParameter("name");
+        String pwd = request.getParameter("pwd");
+        if("张三".equals(name) && "123".equals(pwd)){
+            request.getRequestDispatcher("index.jsp").forward(request,response);
+        }
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doPost(request,response);
+    }
+}
